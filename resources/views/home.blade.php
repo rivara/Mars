8199822f271c8664833803dcdadfde67394515b5
rivara <ricardo.v.ramirez@gmail.com>
2023-09-25 19:26:13 +0200

    #add css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
                         </div>
                     @endif
 
-                    {{ __('You are logged in!') }}
+                    {{ __('You are logged in!') }}zzz
+                  
                 </div>
             </div>
         </div>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -5,43 +5,36 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
         <title>Laravel</title>
-
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,600&display=swap" rel="stylesheet" />
-
-     
+        
         <!-- Scripts -->
         @viteReactRefresh
-        @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-      
+        @vite(['resources/sass/app.scss', 'resources/js/app.js','resources/css/app.css'])
     </head>
-    <body class="antialiased">
-        <div class="relative sm:flex sm:justify-center sm:items-center min-h-screen bg-dots-darker bg-center bg-gray-100 dark:bg-dots-lighter dark:bg-gray-900 selection:bg-red-500 selection:text-white">
-            @if (Route::has('login'))
-                <div class="sm:fixed sm:top-0 sm:right-0 p-6 text-right z-10">
-                    @auth
-                        <a href="{{ url('/home') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Home</a>
-                    @else
-                        <a href="{{ route('login') }}" class="font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Log in</a>
-
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}" class="ml-4 font-semibold text-gray-600 hover:text-gray-900 dark:text-gray-400 dark:hover:text-white focus:outline focus:outline-2 focus:rounded-sm focus:outline-red-500">Register</a>
-                        @endif
-                    @endauth
-                </div>
-            @endif
-
-            <div class="max-w-7xl mx-auto p-6 lg:p-8">
-                
-
-                <div class="mt-16">
-                   
-                            <button class="btn btn-warning">aa</button>
-                            <div id="example"></div>
-
-                </div>    
+    <body>
+        <div  id="wrapping">
+                <nav  class="navbar" >
+                    <ul>
+                        @auth
+                            <li> <a class="nav-link"  href="{{ url('/home') }}" >Home</a></li>
+                        @else
+                            <li><a class="nav-link"  href="{{ route('login') }}">Log in</a></li>
+                            @if (Route::has('register'))
+                                <li> <a class="nav-link"  href="{{ route('register') }}">Register</a></li>
+                            @endif
+                        @endauth
+                    </ul>
+                </nav>
             </div>
-        </div>
+        <br>
+            
+            <div id="hero">
+         
+                    <section class="d-flex flex-column justify-content-center align-items-center">
+                        <div class="container text-center text-md-left" data-aos="fade-up">
+                            <h1>Welcome </h1>
+                        </div>
+                    </section>
+                </div>    
+          
     </body>
 </html>
