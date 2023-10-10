9fe403eb9d677fbe41b8229c40be35a1b7aab06d
rivara <ricardo.v.ramirez@gmail.com>
2023-10-10 19:02:30 +0200

    #advance

--- resources/js/components/Action2.jsx
+++ resources/js/components/Action2.jsx
@@ -1,58 +1,58 @@
 import React, { useState, useEffect } from 'react';
+import Part3  from "./Action3";
 // Endpoints connection
 
+function send(){
+	alert("ss");
+}
 
 
 var myId = "";
-
 const ChildTwo = (props) => {
-  const [featuresapartaments, setFeaturesApartaments] = useState([]);
-  const [features, setFeatures] = useState([]);
+  const [apartaments, setApartaments] = useState([]);
   if (props.id != myId) {
+	
     myId = props.id;
     // Endpoints connection single 
 	 let json=JSON.stringify(props.id);
-	
-	
     axios.get(
-      'http://127.0.0.1:8000/api/apartamentsfeature/' + json
+      'http://127.0.0.1:8000/api/apartamentsfeature/'+json
     ).then(response => {
       const apartaments = response.data
-      setFeaturesApartaments(apartaments)
+      setApartaments(apartaments)
     })
-
-
+	
   }
 
   return (
-		<div class="container">
-			{props.id}
-				<div class="row">
+	<div class="container">
+		<div class="row">
 				{apartaments.map((apartament) => (
-						<div class="col-md-3">
-						<p>{apartament.title}</p>
-						<div class="property-slider">
-								<img src={apartament.image} alt="Image" class="img-fluid" />
-								<div class="property-content">
-										<div class="price mb-2">
-											<span>{apartament.description}</span>
-										</div>
-										<span class="d-block mb-2 text-black-50"> </span>
-										<b>Items</b>
-											<div class="container">
+						<div class="col-md-3 property-item">
+								<div class="property-slider-wrap" >
+										<div class="property-slider">
+											<img src={apartament.image} alt="Image" class="img-fluid" />
+											<div class="property-content">
+												<div class="price mb-2">
+													<span>{apartament.description}</span>
+												</div>
+												<span class="d-block mb-2 text-black-50"> </span>
+												<b>Items</b>
+												<div class="container">
 													<div class="row">
-															{/* <ChildTree id={features.id}/>				 */}
-														</div>
+														<Part3 id={apartament.id} />
 													</div>
+												</div>	
 												<div class="mt-4 ml-10" >
-													<a onClick={handleDownload}  class="btn btn-house ">reserve</a>
-												</div>		
+													<a onClick={send}  class="btn btn-house">reserve</a>
+												</div>
 											</div>
-									</div>
-							</div>
+										</div>
+								</div>
+						</div>			 
 						))}
-				</div>
-		</div>
+			</div>
+	</div>
   );
 };
 export default ChildTwo;
